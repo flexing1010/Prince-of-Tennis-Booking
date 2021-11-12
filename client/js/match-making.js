e801dc46c815e5e5a8e48ac00eb7a87a1f8a7c42
flexing1010 <kjzon@hotmail.com>
2021-11-12 21:57:15 +0900

    File Structuring

---
+++ client/js/match-making.js
@@ -0,0 +1,10 @@
+const matchForm = document.querySelector(".match-making-form");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  location.href = "waiting_room.html";
+
+  console.log("Aa");
+};
+
+matchForm.addEventListener("submit", handleSubmit);
